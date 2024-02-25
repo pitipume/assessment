ca0c22f0ec3e010646fb316c83dff87c237526cf
pitipume <pitipume.boonyawan@gmail.com>
2024-02-25 19:53:51 +0700

    test commit

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/repos/LotteryRepo.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/repos/LotteryRepo.java
@@ -11,5 +11,5 @@ public interface LotteryRepo extends JpaRepository<LotteryEntity, Long> {
     @Query("SELECT l.ticket FROM LotteryEntity l")
     List<String> findAllTicket();
     //test commit
-
+    //test commit
 }
