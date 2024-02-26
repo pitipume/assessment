10da9398fc56364b13a92e8b878620b2b3fced25
pitipume <pitipume.boonyawan@gmail.com>
2024-02-26 16:01:13 +0700

    add spring security for story exp01 unfinished

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/PosttestApplication.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/PosttestApplication.java
@@ -2,8 +2,10 @@ package com.kbtg.bootcamp.posttest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class PosttestApplication {
 
 	public static void main(String[] args) {
