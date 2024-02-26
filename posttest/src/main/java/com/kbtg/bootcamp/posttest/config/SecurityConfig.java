4280d488961a684e2b5a754f1b3e3102850d75e9
pitipume <pitipume.boonyawan@gmail.com>
2024-02-26 17:32:48 +0700

    add spring security config story exp01 unfinished

---
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/config/SecurityConfig.java
@@ -0,0 +1,65 @@
+package com.kbtg.bootcamp.posttest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+        @Bean
+        SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests((requests) -> requests
+                                    .requestMatchers("/admin/**").hasRole("ADMIN")
+                                    .requestMatchers("/**").permitAll()
+                                    .anyRequest()
+                                    .authenticated())
+                    .httpBasic(withDefaults());
+            return http.build();
+        }
+        @Bean
+        public UserDetailsService userDetailsService() {
+            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            UserDetails user = User.withUsername("admin")
+                    .password(passwordEncoder.encode("password"))
+                    .roles("ADMIN")
+                    .build();
+            return new InMemoryUserDetailsManager(user);
+        }
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
+
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService() {
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("admin")
+//                .password("password")
+//                .roles("ADMIN")
+//                .build();
+//        return new InMemoryUserDetailsManager(user);
+//    }
+//
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests()
+//                .requestMatchers("/users/**").permitAll()
+//                .requestMatchers("/admin/**").authenticated();
+//        return http.build();
+//    }
+
+}
